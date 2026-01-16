e8927016ccda8e169803c6b798a4b091829ecae5
GavinClifton <166751534+Gavincli@users.noreply.github.com>
2026-01-16 14:38:58 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Mission2_Assignment;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 0;
+        
+
+        RollDice rd = new RollDice();
+
+        Console.WriteLine("Enter how many times to roll the dice: ");
+        num = int.Parse(Console.ReadLine());
+
+        int[] sums = new int[num];
+
+        for (int i = 0; i < num; i++)
+        {
+            sums[i] = rd.Roll(num);
+                     
+
+        }
+
+        for (int value = 2; value <= 12; value++)
+        {
+
+            int count = 0;
+            foreach (int roll in sums)
+            {
+                
+                if (roll == value)
+                {
+                    count++;
+                    
+                }
+            }
+            int stars = (count * 100) / num;
+
+            Console.Write($"{value}: ");
+            Console.Write(new string('*', stars));
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Thank you for using the dice simulator. Goodbye!");
+
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission2_Assignment
+{
+    internal class RollDice
+    {
+        Random rand = new Random();
+        public int Roll(int rolls)
+        {
+            int dice1 = rand.Next(1, 7);
+            int dice2 = rand.Next(1, 7);
+            int sum = dice1 + dice2;
+            return sum; 
+        }
+    }
+}
